be91f99dd80c263cc5de7fb8334d6a6ba5e7eccc
Parker <event.roleplay.1@mail.ru>
2021-12-11 01:12:36 +0300

    Update index

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const sequelize = require('./database')
+const models = require('./models/models')
+const cors = require('cors')
 
 require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Work!'})
+})
+
+
 
 const start = async () => {
     try {
