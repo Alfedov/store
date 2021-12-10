f31331e52795c7ea5df941ba2bc7360f718d09de
Parker <event.roleplay.1@mail.ru>
2021-12-11 01:06:02 +0300

    Fix database connect New models

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
-const express = require('express');
+const express = require('express')
+const sequelize = require('./database')
+
 require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start()
