e80717610294d743c27dde0bd562f93a038eed5d
Parker <event.roleplay.1@mail.ru>
2021-12-12 10:57:42 +0300

    init

---
+++ client/src/routes.js
@@ -0,0 +1,41 @@
+import Admin from "./pages/Admin";
+import Basket from "./pages/Basket";
+import Shop from "./pages/Shop";
+import Login from "./pages/Auth";
+import Device from "./pages/DevicePage";
+
+import {ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, REGISTER_ROUTE, SHOP_ROUTE} from "./utils/consts";
+
+
+
+
+
+export const authRoutes = [
+    {
+        path: ADMIN_ROUTE,
+        Component: Admin
+    },
+    {
+        path: BASKET_ROUTE,
+        Component: Basket
+    }
+]
+
+export const publicRoutes = [
+    {
+        path: SHOP_ROUTE,
+        Component: Shop
+    },
+    {
+        path: LOGIN_ROUTE,
+        Component: Login
+    },
+    {
+        path: REGISTER_ROUTE,
+        Component: Login
+    },
+    {
+        path: DEVICE_ROUTE + '/:id',
+        Component: Device
+    }
+]
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const fileupload = require('express-fileupload')
 const router = require('./routers/index')
 const errorHandler = require('./midelware/ErrorHandlingMiddleware')
+const path = require('path')
 
 require('dotenv').config()
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileupload({}))
 app.use('/api', router)
 
